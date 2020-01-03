2ce5ff42f67cc75d72c0888ff352e6f0aa1a8bf8
Regan Khadgi <regan.khadgi1@gmail.com>
2020-01-03 21:41:52 +0545

    plugin header added

---
+++ pw-contact-form.php
@@ -0,0 +1,17 @@
+<?php
+
+defined( 'ABSPATH' ) or die( 'No script kiddies please!!' );
+/**
+ * Plugin Name:       PW Contact Form
+ * Plugin URI:        https://github.com/regankhadgi/pw-contact-form
+ * Description:       A demo contact form plugin for the WordPress Plugin Development Workshop
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      5.0
+ * Author:            Regan Khadgi
+ * Author URI:        https://github.com/regankhadgi
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       pw-contact-form
+ * Domain Path:       /languages
+ */
